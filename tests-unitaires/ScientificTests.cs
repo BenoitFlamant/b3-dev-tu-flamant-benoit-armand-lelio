18d0b125796bde9dd0aa3592ab4585a2c88fa012
Ben <benoitflamant1993@gmail.com>
2022-12-18 17:30:35 +0100

    Correctives for ScientificTests : wrong class called

--- tests-unitaires/ScientificTests.cs
+++ tests-unitaires/ScientificTests.cs
@@ -10,28 +10,28 @@ namespace tests_unitaires
         public void SquareTest()
         {
             //Tests for Square in Scientific class
-            Assert.Equal(4, Arithmetic.Square(2));
-            Assert.Equal(0.25, Arithmetic.Square(0.5));
-            Assert.Equal(16, Arithmetic.Square(-4));
+            Assert.Equal(4, Scientific.Square(2));
+            Assert.Equal(0.25, Scientific.Square(0.5));
+            Assert.Equal(16, Scientific.Square(-4));
         }
 
         [Fact]
         public void SquareRootTest()
         {
             //Tests for SquareRoot in Scientific class
-            Assert.Equal(2, Arithmetic.SquareRoot(4));
-            Assert.Equal(0.5, Arithmetic.SquareRoot(0.25));
+            Assert.Equal(2, Scientific.SquareRoot(4));
+            Assert.Equal(0.5, Scientific.SquareRoot(0.25));
         }
 
         [Fact]
         public void PowerTest()
         {
             //Tests for Power in Scientific class
-            Assert.Equal(16, Arithmetic.Power(2,4));
-            Assert.Equal(0.125, Arithmetic.Power(0.5,3));
-            Assert.Equal(-64, Arithmetic.Power(-4,3));
-            Assert.Equal(25, Arithmetic.Power(-5,2));
-            Assert.Equal(0.1, Arithmetic.Power(1,-1));
+            Assert.Equal(16, Scientific.Power(2, 4));
+            Assert.Equal(0.125, Scientific.Power(0.5, 3));
+            Assert.Equal(-64, Scientific.Power(-4, 3));
+            Assert.Equal(25, Scientific.Power(-5, 2));
+            Assert.Equal(0.1, Scientific.Power(1, -1));
         }
     }
 }
