8df13675a485d0bcb22243776563216ae40521c9
Ben <benoitflamant1993@gmail.com>
2022-12-18 16:25:29 +0100

    Arithmetic class added

--- b3-dev-tu-flamant-benoit-armand-lelio/Arithmetic.cs
+++ b3-dev-tu-flamant-benoit-armand-lelio/Arithmetic.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace b3_dev_tu_flamant_benoit_armand_lelio
 {
-    internal class Arithmetic
+    public class Arithmetic
     {
+        //Arithmetic functions for a calculator
+        public static float Addition(float a, float b)
+        {
+            //Do an addition
+            return a + b;
+        }
+
+        public static float Substraction(float a, float b)
+        {
+            //Do a substraction
+            return a - b;
+        }
+
+        public static float Multiplication(float a, float b)
+        {
+            //Do a multiplication
+            return a * b;
+        }
+
+        public static float Division(float a, float b)
+        {
+            //Do a division
+            return a / b;
+        }
+
+        public static float Percentage(float a, float b)
+        {
+            //b% of a
+            return Multiplication(a, b) / 100;
+        }
     }
 }
