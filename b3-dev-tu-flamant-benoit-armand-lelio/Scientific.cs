fb7d74de38dafdf237c2bfff58366b1c3469f370
Ben <benoitflamant1993@gmail.com>
2022-12-18 17:13:07 +0100

    Scientific class added

--- b3-dev-tu-flamant-benoit-armand-lelio/Scientific.cs
+++ b3-dev-tu-flamant-benoit-armand-lelio/Scientific.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace b3_dev_tu_flamant_benoit_armand_lelio
 {
-    internal class Scientific
+    public class Scientific
     {
+        //Arithmetic functions for a calculator
+        public static double Square(double a)
+        {
+            //Return the square of a
+            return a * a;
+        }
+        public static double SquareRoot(double a)
+        {
+            //Return the squareroot of a
+            return Math.Sqrt(a);
+        }
+        public static double Power(double a, double b)
+        {
+            //Return the square of a
+            return Math.Pow(a, b);
+        }
     }
 }
